82ffae80e1891bac5cefcd594215bd63ca5c6eff
Glitch (slime-board) <none>
2019-02-04 15:50:00 +0000

    🎺😜 Checkpoint ./routes/api.js:2619925/2917

--- routes/api.js
+++ routes/api.js
@@ -12,33 +12,42 @@ var expect = require('chai').expect;
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
 const MONGODB_CONNECTION_STRING = process.env.DB;
+let db
 //Example connection: MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, db) {});
 
 module.exports = function (app) {
 
-  MongoClient.connect(process.env.DB, (err, db) => {
+  MongoClient.connect(MONGODB_CONNECTION_STRING, (err, dba) => {
     
     if (err) 
       console.log('Error Connecting to Database')
     else {
-      
       console.log('Successfully Connected to a Database')
-      
-      app.route('/api/books')
+      db = dba            
+    }
+    
+  })
+  
+  app.route('/api/books')
         .get(function (req, res){
           //response will be array of book objects
           //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+          let books = db.collection('books').find()
+          console.log(books)
+          res.json(books.toArray())
+          
+    
         })
 
         .post(function (req, res){
           var title = req.body.title;
           //response will contain new book object including atleast _id and title
-          db.collection('books').insertOne({title: title}, (err, doc) => {
+          db.collection('books').insert({title: title}, (err, doc) => {
             if (err) {
               console.log(err)
               res.json({'error': 'Error in Adding Book'})
             } else {
-              res.json({doc})
+              res.json(doc.ops[0])
             }
           })
         })
@@ -65,9 +74,5 @@ module.exports = function (app) {
           var bookid = req.params.id;
           //if successful response will be 'delete successful'
         });
-      
-    }
-    
-  })
   
 };
